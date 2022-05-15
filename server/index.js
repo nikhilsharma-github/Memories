67dd1aa8533ba766526c3cd03b7d8d07f199c4e3
Nikhil Sharma <nikhilsharma.csgeek@gmail.com>
2022-05-16 00:14:51 +0530

    Commit 2 before starting auth

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 // const express=require('express');
 
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -15,13 +17,13 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://ProjectUser1:projectuser1@cluster0.nho8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://ProjectUser1:projectuser1@cluster0.nho8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`))
   )
